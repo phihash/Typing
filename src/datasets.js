6d38f8e959b17625b2b9e7a47b4e70090172bfc4
phihash <insharp0220@gmail.com>
2022-01-20 12:20:36 +0900

    データ構造変更

--- src/datasets.js
+++ src/datasets.js
@@ -1,6 +1,5 @@
-export const datasets = {
-    1:["貧血","anemia"],
-    2:["下痢","diarrhea"],
-    3:["緑内障","glaucoma"],
-    4:["食思不振","anorexia"],
-}
+export const datasets = [
+    {1:["貧血","anemia"]},
+    {2:["食思不振","anorexia"]},
+    {3:["緑内障","glaucoma"]}
+];
