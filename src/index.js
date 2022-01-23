a45395fd938cc9a25cf7a8ebe1c1016664fb8e58
Mail I <maili@merunoMacBook-Air.local>
2022-01-23 13:49:34 +0900

    html,css編集

--- src/index.js
+++ src/index.js
@@ -16,12 +16,29 @@ export let questionNumber = 10; //デフォルト値
 export let remainingQuestionNumber = 10; //残りの問題数
 export const typingArea = document.getElementById("typingArea");
 export const questionNumberArea = document.getElementById("questionNumberArea");
+export const settingSaveButton = document.getElementById("settingSaveButton");
+export const eToJButton = document.getElementById("eToJButton");
+export const jToEButton = document.getElementById("jToEButton");
 
 
 MicroModal.init({
   awaitOpenAnimation: true,
 });
 
+settingSaveButton.addEventListener("click",startTyping);
+
+eToJButton.addEventListener("click",changeButtonColor);
+jToEButton.addEventListener("click",changeButtonColor);
+
+const changeButtonColor = () => {
+
+}
+
+const startTyping = () => {
+  //問題数を設定
+  
+  console.log(questionNumber);
+}
 
 typingArea.addEventListener("keypress",(e) => {
   if(e.key == "Enter"){
